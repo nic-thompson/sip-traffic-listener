e2c0bb4b88998ca43f73fcfe36f15d16e95c825e
Nic Thompson <mail@nicolasthompson.com>
2024-10-12 22:58:26 +0100

    chore: change filter to udp

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
-import { PacketCaptureModule } from "../src/interceptor/packetCapture";
+import { PacketCaptureModule } from '../src/interceptor/packetCapture';
 
-const networkInterface = "eth2";
-const filter = "tcp port 5060";
+const networkInterface = 'eth2';
+const filter = 'udp port 5060';
 
 try {
-  const packetCapture = new PacketCaptureModule(networkInterface, filter);
-  console.log(
-    `Starting packet capture on ${networkInterface} with filter ${filter}`
-  );
-  packetCapture.start();
+    const packetCapture = new PacketCaptureModule(networkInterface, filter);
+    console.log(
+        `Starting packet capture on ${networkInterface} with filter ${filter}`
+    );
+    packetCapture.start();
 } catch (error) {
-  if (error instanceof Error) {
-    console.error(`Failed to start packet capture: ${error.message}`);
-  } else {
-    console.error(`Failed to start packet capture: Unknown error`);
-  }
+    if (error instanceof Error) {
+        console.error(`Failed to start packet capture: ${error.message}`);
+    } else {
+        console.error(`Failed to start packet capture: Unknown error`);
+    }
 }
-
